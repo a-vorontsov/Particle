51b99a2048e0fcc2ac25bc0031311bb1ed44e271
Aleksandr Vorontsov <vorontsov98@gmail.com>
2017-01-29 16:32:38 +0000

    Add initial Unity files

---
+++ GameDevPlatformer/Assets/Scripts/Controller2D.cs
@@ -0,0 +1,214 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (BoxCollider2D))]
+public class Controller2D : MonoBehaviour {
+
+	public LayerMask collisionMask;
+
+	const float skinWidth = 0.015f;
+
+	public int horizontalRayCount = 4;
+	public int verticalRayCount = 4;
+
+	float maxClimbAngle = 75;
+	float maxDescendAngle = 80;
+	float horizontalRaySpacing;
+	float verticalRaySpacing;
+
+	BoxCollider2D collider;
+	RaycastOrigins raycastOrigins;
+
+	public CollisionInfo collisions;
+
+	void Start () {
+		collider = GetComponent<BoxCollider2D> ();
+		CalculateRaySpacing ();
+		collisions.faceDirection = 1;
+	}
+
+	public void Move(Vector2 velocity) {
+		UpdateRaycastOrigins ();
+		collisions.Reset ();
+
+		if (velocity.x != 0) {
+			collisions.faceDirection = (int)Mathf.Sign (velocity.x);
+		}
+
+		if (velocity.y < 0) {
+			DescendSlope (ref velocity);
+		}
+
+		HorizontalCollisions (ref velocity);
+
+		if (velocity.y != 0) {
+			VerticalCollisions (ref velocity);
+		}
+
+		transform.Translate (velocity);
+	}
+
+	void HorizontalCollisions(ref Vector2 velocity) {
+		float directionX = collisions.faceDirection;
+		float rayLength = Mathf.Abs (velocity.x) + skinWidth;
+
+		if (Mathf.Abs (velocity.x) < skinWidth) {
+			rayLength = 2 * skinWidth;
+		}
+
+		for (int i = 0; i < horizontalRayCount; i ++) {
+			Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+			rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+			RaycastHit2D hit = Physics2D.Raycast (rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+			Debug.DrawRay (rayOrigin, Vector2.right * directionX * rayLength, Color.red);
+
+			if (hit) {
+				float slopeAngle = Vector2.Angle (hit.normal, Vector2.up);
+				if (i == 0 && slopeAngle <= maxClimbAngle) {
+					float distanceToSlopeStart = 0;
+					if (slopeAngle != collisions.slopeAngleOld) {
+						distanceToSlopeStart = hit.distance - skinWidth;
+						velocity.x -= distanceToSlopeStart * directionX;
+					}
+
+					ClimbSlope (ref velocity, slopeAngle);
+					velocity.x += distanceToSlopeStart * directionX;
+				}
+
+				if (!collisions.climbingSlope || slopeAngle > maxClimbAngle) {
+					velocity.x = (hit.distance - skinWidth) * directionX;
+					rayLength = hit.distance;
+					if (collisions.climbingSlope) {
+						velocity.y = Mathf.Tan (collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Abs(velocity.x);
+					}
+
+					collisions.left = directionX == -1;
+					collisions.right = directionX == 1;
+				}
+			}
+		}
+	}
+
+	void VerticalCollisions(ref Vector2 velocity) {
+		float directionY = Mathf.Sign (velocity.y);
+		float rayLength = Mathf.Abs (velocity.y) + skinWidth;
+
+		for (int i = 0; i < verticalRayCount; i ++) {
+			Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+			rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
+			RaycastHit2D hit = Physics2D.Raycast (rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
+			Debug.DrawRay (rayOrigin, Vector2.up * directionY * rayLength, Color.red);
+			if (hit) {
+				velocity.y = (hit.distance - skinWidth) * directionY;
+				rayLength = hit.distance;
+				if (collisions.climbingSlope) {
+					velocity.x = velocity.y / Mathf.Tan (collisions.slopeAngle * Mathf.Deg2Rad) * Mathf.Sign (velocity.x);
+				}
+
+				collisions.below = directionY == -1;
+				collisions.above = directionY == 1;
+			}
+		}
+		if (collisions.climbingSlope) {
+			float directionX = Mathf.Sign (velocity.x);
+			rayLength = Mathf.Abs (velocity.x) + skinWidth;
+			Vector2 rayOrigin = ((directionX == -1)?raycastOrigins.bottomLeft:raycastOrigins.bottomRight) + Vector2.up * velocity.y;
+			RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collisionMask);
+			if (hit) {
+				float slopeAngle = Vector2.Angle(hit.normal, Vector2.up);
+				if (slopeAngle != collisions.slopeAngle) {
+					velocity.x = (hit.distance - skinWidth) * directionX;
+					collisions.slopeAngle = slopeAngle;
+				}
+			}
+		}
+	}
+
+	void ClimbSlope (ref Vector2 velocity, float slopeAngle) {
+		float moveDistance = Mathf.Abs (velocity.x);
+		float climbVelocityY = Mathf.Sin (slopeAngle * Mathf.Deg2Rad) * moveDistance;
+
+		if (velocity.y <= climbVelocityY) {
+			velocity.y = climbVelocityY;
+			velocity.x = Mathf.Cos (slopeAngle * Mathf.Deg2Rad) * moveDistance * Mathf.Sign(velocity.x);
+
+			collisions.below = true;
+			collisions.climbingSlope = true;
+			collisions.slopeAngle = slopeAngle;
+		}
+	}
+
+	void DescendSlope (ref Vector2 velocity) {
+		float directionX = Mathf.Sign (velocity.x);
+
+		Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomRight : raycastOrigins.bottomLeft;
+		RaycastHit2D hit = Physics2D.Raycast (rayOrigin, -Vector2.up, Mathf.Infinity, collisionMask);
+
+		if (hit) {
+			float slopeAngle = Vector2.Angle (hit.normal, Vector2.up);
+			if (slopeAngle != 0 && slopeAngle <= maxDescendAngle) {
+				if (Mathf.Sign (hit.normal.x) == directionX) {
+					if (hit.distance - skinWidth <= Mathf.Tan (slopeAngle * Mathf.Deg2Rad) * Mathf.Abs (velocity.x)) {
+						float moveDistance = Mathf.Abs (velocity.x);
+						float descendVelocityY = Mathf.Sin (slopeAngle * Mathf.Deg2Rad) * moveDistance;
+
+						velocity.x = Mathf.Cos (slopeAngle * Mathf.Deg2Rad) * moveDistance * Mathf.Sign(velocity.x);
+						velocity.y -= descendVelocityY;
+
+						collisions.slopeAngle = slopeAngle;
+						collisions.descendingSlope = true;
+						collisions.below = true;
+					}
+				}
+			}
+		}
+	}
+
+	void UpdateRaycastOrigins(){
+		Bounds bounds = collider.bounds;
+		bounds.Expand (skinWidth * -2);
+
+		raycastOrigins.bottomLeft = new Vector2 (bounds.min.x, bounds.min.y);
+		raycastOrigins.bottomRight = new Vector2 (bounds.max.x, bounds.min.y);
+		raycastOrigins.topLeft = new Vector2 (bounds.min.x, bounds.max.y);
+		raycastOrigins.topRight = new Vector2 (bounds.max.x, bounds.max.y);
+	}
+
+	void CalculateRaySpacing(){
+		Bounds bounds = collider.bounds;
+		bounds.Expand (skinWidth * -2);
+
+		horizontalRayCount = Mathf.Clamp (horizontalRayCount, 2, int.MaxValue);
+		verticalRayCount = Mathf.Clamp (verticalRayCount, 2, int.MaxValue);
+
+		horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+		verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+	}
+
+	// Set up origins of collision rays
+	struct RaycastOrigins {
+		public Vector2 topLeft, topRight;
+		public Vector2 bottomLeft, bottomRight;
+	}
+		
+	public struct CollisionInfo {
+		public bool above, below;
+		public bool left, right;
+		public bool climbingSlope;
+		public bool descendingSlope;
+
+		public float slopeAngle, slopeAngleOld;
+
+		public int faceDirection;
+
+		public void Reset() {
+			above = below = false;
+			left = right = false;
+			climbingSlope = false;
+			descendingSlope = false;
+			slopeAngleOld = slopeAngle;
+			slopeAngle = 0;
+		}
+	}
+}
---
+++ GameDevPlatformer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Controller2D))]
+public class PlayerMovement : MonoBehaviour {
+
+	public float jumpHeight = 2;
+	public float timeToJumpApex = 0.3f;
+	public float wallSlideSpeedMax = 0;
+	public float wallStickTime = 0.05f;
+	public float timeToWallUnstick;
+
+	public Vector2 wallClimb;
+	public Vector2 wallHop;
+	public Vector2 wallLeap;
+
+	float accelerationTimeAirborne = 0.2f;
+	float accelerationTimeGrounded = 0.1f;
+	float moveSpeed = 6;
+	float gravity;
+	float jumpVelocity;
+	float velocityXSmoothing;
+
+	int doubleJump = 0;
+
+	bool wallSliding = false;
+	bool canWallSlide = true;
+
+	Vector2 velocity;
+	Controller2D controller;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<Controller2D> ();
+		// calculate gravity and jump strength
+		gravity = -(2 * jumpHeight) / Mathf.Pow (timeToJumpApex, 2);
+		jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+	}
+
+	public IEnumerator stopWallSlide() {
+		yield return new WaitForSeconds (0.5f);
+		wallSliding = false;
+		canWallSlide = false;
+		yield return new WaitUntil(() => controller.collisions.below || controller.collisions.left || controller.collisions.right);
+		canWallSlide = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// Movement input
+		Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+		int wallDirectionX = (controller.collisions.left) ? -1 : 1;
+
+		float targetVelocityX = input.x * moveSpeed;
+		// Reduce horizontal acceleration in mid air
+		velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
+
+		if ((controller.collisions.left || controller.collisions.right) && !controller.collisions.below && velocity.y < 0 && canWallSlide == true) {
+			wallSliding = true;
+			if (velocity.y < -wallSlideSpeedMax) {
+				velocity.y = -wallSlideSpeedMax;
+			}
+
+			if (timeToWallUnstick > 0) {
+				velocityXSmoothing = 0;
+				velocity.x = 0;
+				if (input.x != wallDirectionX && input.x != 0) {
+					timeToWallUnstick -= Time.deltaTime;
+				} 
+				else {
+					timeToWallUnstick = wallStickTime;
+				}
+			} 
+			else {
+				timeToWallUnstick = wallStickTime;
+			}
+			StartCoroutine (stopWallSlide ());
+		}
+
+		// Stop gravity build up while grounded
+		if (controller.collisions.above || controller.collisions.below) {
+			velocity.y = 0;
+		}
+
+		// Jump and double jump input and check
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			if (wallSliding) {
+				if (wallDirectionX == -input.x) {
+					velocity.x = -wallDirectionX * wallLeap.x;
+					velocity.y = wallLeap.y;
+					doubleJump = 2;
+					canWallSlide = true;
+				} 
+				else {
+					velocity.x = -wallDirectionX * wallHop.x;
+					velocity.y = wallHop.y;
+					doubleJump = 2;
+					canWallSlide = true;
+				} 
+			}
+			//Recalculate gravity and jump strength mid air
+			if (doubleJump != 2) {
+				gravity = -(2 * jumpHeight) / Mathf.Pow (timeToJumpApex, 2);
+				jumpVelocity = Mathf.Abs (gravity) * timeToJumpApex;
+				velocity.y = jumpVelocity;
+				doubleJump += 1;
+				jumpHeight = 1.5f;
+			}
+		}
+
+		// Double jump reset on landing
+		if (controller.collisions.below && velocity.y == 0) {
+			doubleJump = 0;
+			jumpHeight = 2;
+		}
+			
+		velocity.y += gravity * Time.deltaTime;
+		controller.Move (velocity * Time.deltaTime);
+	}
+}
