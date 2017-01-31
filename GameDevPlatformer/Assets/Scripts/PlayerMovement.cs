b2a625ad1dc114e23c958242eee60c808ce856ee
Aleksandr Vorontsov <vorontsov98@gmail.com>
2017-01-31 19:50:47 +0000

    Modify air acceleration, Add ground deceleration timer

--- GameDevPlatformer/Assets/Scripts/PlayerMovement.cs
+++ GameDevPlatformer/Assets/Scripts/PlayerMovement.cs
@@ -10,13 +10,14 @@ public class PlayerMovement : MonoBehaviour {
 	public float wallSlideSpeedMax = 0;
 	public float wallStickTime = 0.05f;
 	public float timeToWallUnstick;
+	public float timeToGroundDecelerate;
 
 	public Vector2 wallHop;
 	public Vector2 wallLeap;
 
 	float accelerationTimeGrounded = 0.1f;
-	float moveSpeed = 5;
-	float minMoveSpeed = 5;
+	float moveSpeed = 10;
+	float minMoveSpeed = 10;
 	float gravity;
 	float jumpVelocity;
 	float velocityXSmoothing;
@@ -24,29 +25,37 @@ public class PlayerMovement : MonoBehaviour {
 	Vector2 velocity;
 	Controller2D controller;
 
-	IEnumerator Wait() {
-		yield return new WaitForSeconds (2f);
-	}
-
 	void Start () {
 		controller = GetComponent<Controller2D> ();
 		// calculate gravity and jump strength
 		gravity = -(2 * jumpHeight) / Mathf.Pow (timeToJumpApex, 2);
 		jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
 	}
+
+	IEnumerator GroundTime() {
+		if (controller.collisions.below) {
+			Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+			Debug.Log (timeToGroundDecelerate);
+			yield return new WaitUntil (() => timeToGroundDecelerate == 2);
+			timeToGroundDecelerate += Time.deltaTime;
+			velocity.x = Mathf.SmoothDamp (velocity.x, (input.x * moveSpeed), ref velocityXSmoothing, accelerationTimeGrounded);
+
+		}
+	}
 		
 	void Update () {
-		print (velocity.x);
 		// Movement input
 		Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
 		int wallDirectionX = (controller.collisions.left) ? -1 : 1;
 		float targetVelocityXGround = input.x * moveSpeed;
 		// Reduce horizontal acceleration on ground after waiting
 		if (controller.collisions.below) {
-			
-			StartCoroutine (Wait ());
+			if (timeToGroundDecelerate != 2) {
+				StartCoroutine (GroundTime ());
+			} 
 			velocity.x = Mathf.SmoothDamp (velocity.x, targetVelocityXGround, ref velocityXSmoothing, accelerationTimeGrounded);
 		} 
+
 		// Accelerate while airbourne
 		else {
 			if (velocity.x > 0) {
@@ -92,10 +101,11 @@ public class PlayerMovement : MonoBehaviour {
 			}
 		}
 				
+		Debug.Log (input.x);
 
 		// Jump and double jump input and check
 		if (Input.GetKeyDown (KeyCode.Space)) {
-			velocity.x += 10 * Time.deltaTime;
+			velocity.x += Mathf.Sign(input.x) * Mathf.Pow(Time.deltaTime, 2);
 			if (wallSliding) {
 				if ( wallDirectionX == 0) {
 					velocity.x = -wallDirectionX * wallHop.x;
