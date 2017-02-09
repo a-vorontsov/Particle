c1c2dd0025e0035b4d771587fec456bee85d9ac5
Aleksandr Vorontsov <vorontsov98@gmail.com>
2017-02-09 18:15:37 +0000

    Add following camera
    Unity update added a large number of (un)necessary libraries to the folder. New commit is larger than expected.

---
+++ GameDevPlatformer/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Controller2D target;
+	public Vector2 focusAreaSize;
+	public Vector2 velocity;
+	public Vector3 minCameraPos;
+	public Vector3 maxCameraPos;
+
+	public float verticalOffset;
+	public float lookAheadDistanceX;
+	public float lookSmoothTimeX;
+	public float verticalSmoothTime;
+
+	public bool bounds;
+
+	FocusArea focusArea;
+
+	float currentLookAheadX;
+	float targetLookAheadX;
+	float lookAheadDirectionX;
+	float smoothLookVelocityX;
+	float smoothVelocityY;
+
+	bool lookAheadStop;
+
+	void Start() {
+		focusArea = new FocusArea (target.collider.bounds, focusAreaSize);
+	}
+
+	void LateUpdate() {
+		focusArea.Update (target.collider.bounds);
+		Vector2 focusPosition = focusArea.centre + Vector2.up * verticalOffset;
+
+		if (focusArea.velocity.x != 0) {
+			lookAheadDirectionX = Mathf.Sign (focusArea.velocity.x);
+			if (Mathf.Sign (target.playerInput.x) == Mathf.Sign (focusArea.velocity.x) && target.playerInput.x != 0) {
+				lookAheadStop = false;
+				targetLookAheadX = lookAheadDirectionX * lookAheadDistanceX;
+			} 
+			else {
+				if (!lookAheadStop) {
+					lookAheadStop = true;
+					targetLookAheadX = currentLookAheadX + (lookAheadDirectionX * lookAheadDistanceX - currentLookAheadX) / 4f;
+				}
+			}
+		}
+		currentLookAheadX = Mathf.SmoothDamp (currentLookAheadX, targetLookAheadX, ref smoothLookVelocityX, lookSmoothTimeX);
+
+		focusPosition.y = Mathf.SmoothDamp (transform.position.y, focusPosition.y, ref smoothVelocityY, verticalSmoothTime);
+		focusPosition += Vector2.right * currentLookAheadX;
+
+		transform.position = (Vector3)focusPosition + Vector3.forward * -10;
+
+		if (bounds) {
+			transform.position = new Vector3(
+				Mathf.Clamp(transform.position.x, minCameraPos.x, maxCameraPos.x), 
+				Mathf.Clamp(transform.position.y, minCameraPos.y, maxCameraPos.y), 
+				Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z)
+			);
+		}
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.color = new Color (1, 0, 0, 0.5f);
+		Gizmos.DrawCube (focusArea.centre, focusAreaSize);
+	}
+
+	struct FocusArea {
+		public Vector2 centre;
+		public Vector2 velocity;
+
+		float left,right;
+		float top,bottom;
+
+
+		public FocusArea(Bounds targetBounds, Vector2 size) {
+			left = targetBounds.center.x - size.x/2;
+			right = targetBounds.center.x + size.x/2;
+			bottom = targetBounds.min.y;
+			top = targetBounds.min.y + size.y;
+
+			velocity = Vector2.zero;
+			centre = new Vector2((left+right)/2,(top +bottom)/2);
+		}
+
+		public void Update(Bounds targetBounds) {
+			float shiftX = 0;
+
+			if (targetBounds.min.x < left) {
+				shiftX = targetBounds.min.x - left;
+			} 
+			else if (targetBounds.max.x > right) {
+				shiftX = targetBounds.max.x - right;
+			}
+
+			left += shiftX;
+			right += shiftX;
+
+			float shiftY = 0;
+
+			if (targetBounds.min.y < bottom) {
+				shiftY = targetBounds.min.y - bottom;
+			} 
+			else if (targetBounds.max.y > top) {
+				shiftY = targetBounds.max.y - top;
+			}
+
+			top += shiftY;
+			bottom += shiftY;
+			centre = new Vector2((left+right)/2,(top +bottom)/2);
+			velocity = new Vector2 (shiftX, shiftY);
+		}
+	}
+}
