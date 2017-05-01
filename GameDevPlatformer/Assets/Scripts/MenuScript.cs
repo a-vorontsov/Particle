f8c5494e3017206c737858f8601230d6e4f553e4
Aleksandr Vorontsov <vorontsov98@gmail.com>
2017-05-01 16:15:52 +0100

    Add Player prefs save, Modify ReadMe

--- GameDevPlatformer/Assets/Scripts/MenuScript.cs
+++ GameDevPlatformer/Assets/Scripts/MenuScript.cs
@@ -27,38 +27,55 @@ public class MenuScript : MonoBehaviour {
 
 	int activeResIndex;
 
+	// Force add listeners to objects
 	void OnEnable () {
-		fullScreenToggle.onValueChanged.AddListener (delegate {OnFullScreenToggle();});
-		resolutionDropdown.onValueChanged.AddListener (delegate {OnResolutionChange(resolutionDropdown.value);});
+		fullScreenToggle.onValueChanged.AddListener (delegate {OnFullScreenToggle ();});
+		resolutionDropdown.onValueChanged.AddListener (delegate {OnResolutionChange (resolutionDropdown.value);});
 		masterSlider.onValueChanged.AddListener (delegate {OnVolumeChange();});
 		musicSlider.onValueChanged.AddListener (delegate {OnVolumeChange();});
 		fxSlider.onValueChanged.AddListener (delegate {OnVolumeChange();});
+		activeResIndex = PlayerPrefs.GetInt ("screen res");
+		bool fullScreen = (PlayerPrefs.GetInt ("fullscreen") == 1) ? true : false;
+		AudioListener.volume = masterSlider.value = PlayerPrefs.GetFloat ("master volume");
+		musicSource.volume = musicSlider.value = PlayerPrefs.GetFloat ("music volume");
+		fxSource.volume = fxSlider.value = PlayerPrefs.GetFloat ("effects volume");
 	}
 
 	void Start () {
+		activeResIndex = PlayerPrefs.GetInt ("screen res");
+		bool fullScreen = (PlayerPrefs.GetInt ("fullscreen") == 1) ? true : false;
+		AudioListener.volume = masterSlider.value = PlayerPrefs.GetFloat ("master volume");
+		musicSource.volume = musicSlider.value = PlayerPrefs.GetFloat ("music volume");
+		fxSource.volume = fxSlider.value = PlayerPrefs.GetFloat ("effects volume");
 		mainMenuCanvas.SetActive (true);
 		settingsCanvas.SetActive (false);
 		selectLevelCanvas.SetActive (false);
-
 		activeResIndex = resolutionDropdown.value;
 	}
 
 	// Toggle fullscreen
 	public void OnFullScreenToggle () {
 		Screen.fullScreen = fullScreenToggle.isOn;
-
+		PlayerPrefs.SetInt ("fullscreen", ((fullScreen) ? 1 : 0));
+		PlayerPrefs.Save ();
 	}
 
 	// Change resolution
 	public void OnResolutionChange (int activeResIndex) {
 		Screen.SetResolution(screenWidths[resolutionDropdown.value], screenHeights[resolutionDropdown.value], Screen.fullScreen);
+		PlayerPrefs.SetInt ("screen res", activeResIndex);
+		PlayerPrefs.Save ();
 	}
 
-	// change volume
+	// Change volume
 	public void OnVolumeChange () {
 		AudioListener.volume = masterSlider.value;
+		PlayerPrefs.SetFloat ("master volume", masterSlider.value);
 		musicSource.volume = musicSlider.value;
+		PlayerPrefs.SetFloat ("music volume", musicSlider.value);
 		fxSource.volume = fxSlider.value;
+		PlayerPrefs.SetFloat ("effects volume", fxSlider.value);
+		PlayerPrefs.Save ();
 	}
 
 	// Start game on press
@@ -85,6 +102,7 @@ public class MenuScript : MonoBehaviour {
 		selectLevelCanvas.SetActive (false);
 	}
 
+	// Play sound on click
 	public void ClickSound () {
 		fxSource.Play ();
 	}
@@ -92,5 +110,6 @@ public class MenuScript : MonoBehaviour {
 	// Quit game on press (doesn't function in unity editor)
 	public void ClickExit () {
 		Application.Quit ();
+		PlayerPrefs.Save();
 	}
 }
